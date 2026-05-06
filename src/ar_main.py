a04a6132c7984f08139c6ed26e26c469ea0ed6c0
Vishw Gotmare <139032652+vishw-g@users.noreply.github.com>
2026-05-06 22:16:29 +0530

    Add files via upload

--- src/ar_main.py
+++ src/ar_main.py
@@ -8,29 +8,39 @@
 #      -> Refactor and organize code (proper funcition definition and separation, classes, error handling...)
 
 import argparse
+import time
+import math
+import os
 
 import cv2
 import numpy as np
-import math
-import os
-from objloader_simple import *
+from objloader_simple import OBJ
 
-# Minimum number of matches that have to be found
-# to consider the recognition valid
-MIN_MATCHES = 10
-DEFAULT_COLOR = (0, 0, 0)
+from utils.cv_utils import rescale_frame
+from utils.data_utils import save_data, get_projected_corners
+
+from kalman import KalmanTracker
+# from utils.corner_select import selector
+from constants import DATA_HEADERS, KALMAN_DATA_HEADERS, MIN_MATCHES, SCALE, DEFAULT_COLOR, DATA_FILE
 
 
 def main():
     """
     This functions loads the target surface image,
     """
-    homography = None 
-    # matrix of camera parameters (made up but works quite well for me) 
+    kalman_filter = KalmanTracker()
+    last_time = 0
+    # Flag to initialize the kalman filter
+    FIRST_ITERATION = True
+    # selector('reference/model.jpg')
+    kalman_frame = None
+
+    homography = None
+    # matrix of camera parameters (made up but works quite well for me)
     camera_parameters = np.array([[800, 0, 320], [0, 800, 240], [0, 0, 1]])
     # create ORB keypoint detector
     orb = cv2.ORB_create()
-    # create BFMatcher object based on hamming distance  
+    # create BFMatcher object based on hamming distance
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     # load the reference surface that will be searched in the video stream
     dir_name = os.getcwd()
@@ -38,63 +48,145 @@ def main():
     # Compute model keypoints and its descriptors
     kp_model, des_model = orb.detectAndCompute(model, None)
     # Load 3D model from OBJ file
-    obj = OBJ(os.path.join(dir_name, 'models/fox.obj'), swapyz=True)  
+    obj = OBJ(os.path.join(dir_name, 'models/fox.obj'), swapyz=True)
     # init video capture
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0) # From camera
+    cap = cv2.VideoCapture('IMG_5609.mp4')  # From video
+
+    current_frame_index = 1  # for indexing stored data
+    data_headers = DATA_HEADERS
+    if args.filtering:
+        data_headers += KALMAN_DATA_HEADERS
+
+    data_to_save = []
 
     while True:
         # read the current frame
-        ret, frame = cap.read()
-        if not ret:
-            print("Unable to capture video")
-            return 
-        # find and draw the keypoints of the frame
-        kp_frame, des_frame = orb.detectAndCompute(frame, None)
-        # match frame descriptors with model descriptors
-        matches = bf.match(des_model, des_frame)
-        # sort them in the order of their distance
-        # the lower the distance, the better the match
-        matches = sorted(matches, key=lambda x: x.distance)
+        try:
+            ret, frame = cap.read()
+            frame = rescale_frame(frame, scale=SCALE)
+            if not ret:
+                print("Unable to capture video")
+                return
+            # find and draw the keypoints of the frame
+            kp_frame, des_frame = orb.detectAndCompute(frame, None)
+            # match frame descriptors with model descriptors
+            matches = bf.match(des_model, des_frame)
+            # sort them in the order of their distance
+            # the lower the distance, the better the match
+            matches = sorted(matches, key=lambda x: x.distance)
 
-        # compute Homography if enough matches are found
-        if len(matches) > MIN_MATCHES:
-            # differenciate between source points and destination points
-            src_pts = np.float32([kp_model[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-            dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-            # compute Homography
-            homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            if args.rectangle:
-                # Draw a rectangle that marks the found model in the frame
-                h, w = model.shape
-                pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-                # project corners into frame
-                dst = cv2.perspectiveTransform(pts, homography)
-                # connect them with lines  
-                frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)  
-            # if a valid homography matrix was found render cube on model plane
-            if homography is not None:
-                try:
-                    # obtain 3D projection matrix from homography matrix and camera parameters
-                    projection = projection_matrix(camera_parameters, homography)  
-                    # project cube or model
-                    frame = render(frame, obj, projection, model, False)
-                    #frame = render(frame, model, projection)
-                except:
-                    pass
-            # draw first 10 matches.
-            if args.matches:
-                frame = cv2.drawMatches(model, kp_model, frame, kp_frame, matches[:10], 0, flags=2)
-            # show result
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            # compute Homography if enough matches are found
+            if len(matches) > MIN_MATCHES:
+                # differenciate between source points and destination points
+                src_pts = np.float32(
+                    [kp_model[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+                dst_pts = np.float32(
+                    [kp_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+                # compute Homography
+                homography, _ = cv2.findHomography(
+                    src_pts, dst_pts, cv2.RANSAC, 5.0)
+                if args.rectangle or args.filtering or args.save:
+                    # Draw a rectangle that marks the found model in the frame
+                    h, w = model.shape
+                    # tl, bl, br, tr
+                    pts = np.float32(
+                        [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+                    center_pts = np.float32([[w/2, h/2]]).reshape(-1, 1, 2)
+                    # project corners into frame
+                    dst = cv2.perspectiveTransform(pts, homography)
+                    p_center_pts = cv2.perspectiveTransform(
+                        center_pts, homography)
+                    # DATA TO SAVE
+                    current_frame_data = [current_frame_index, len(
+                        matches), *p_center_pts[0][0], *get_projected_corners(dst)]
+                    current_frame_index += 1
 
-        else:
-            print("Not enough matches found - %d/%d" % (len(matches), MIN_MATCHES))
+                    if args.filtering:
+                        measured_corners = dst.flatten()  # initial position state or measurments
+                        if FIRST_ITERATION:
+                            state = np.concatenate(
+                                [measured_corners, np.zeros(8)])
+                            # TODO: Revisit values
+                            covariance_matrix = np.eye(16)*0.8
+                            kalman_filter.init(state, covariance_matrix)
+                            last_time = time.time()
+                            FIRST_ITERATION = False
+                        else:
+                            # predict and correct
+                            current_time = time.time()
+                            deltat = current_time - last_time
+                            kalman_filter.predict(dt=deltat)
+                            kalman_filter.correct(measured_corners)
+                            last_time = current_time
+                        # recompute homography
+                        kalman_estimated_corners = kalman_filter.get_current_state()[
+                            0:8].reshape(-1, 1, 2)
+                        kalman_homography, _ = cv2.findHomography(
+                            pts, kalman_estimated_corners, cv2.RANSAC, 5.0)
+                        kalman_frame = frame.copy()
+                        kalman_projected_corners = cv2.perspectiveTransform(
+                            pts, kalman_homography)
+                        kalman_frame = cv2.polylines(kalman_frame, [np.int32(
+                            kalman_projected_corners)], True, 0, 3, cv2.LINE_AA)
+
+                        if args.save:
+                            k_center = cv2.perspectiveTransform(
+                                center_pts, kalman_homography)
+                            k_corners = get_projected_corners(
+                                kalman_projected_corners)
+                            data_to_save.append(
+                                current_frame_data + list(k_center[0][0]) + k_corners)
+                        else:  # ?
+                            data_to_save.append(current_frame_data)
+                    # connect them with lines
+                    frame = cv2.polylines(
+                        frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+                # if a valid homography matrix was found render cube on model plane
+                if homography is not None:
+                    try:
+                        # obtain 3D projection matrix from homography matrix and camera parameters
+                        projection = projection_matrix(
+                            camera_parameters, homography)
+                        # project cube or model
+                        frame = render(frame, obj, projection, model, False)
+                        if args.filtering:
+                            proj_kalman = projection_matrix(
+                                camera_parameters, kalman_homography)
+                            kalman_frame = render(
+                                kalman_frame, obj, proj_kalman, model, False)
+                            both = np.concatenate(
+                                (frame, kalman_frame), axis=0)
+                        #frame = render(frame, model, projection)
+                    except:
+                        pass
+
+                # draw first 10 matches.
+                if args.matches:
+                    frame = cv2.drawMatches(
+                        model, kp_model, frame, kp_frame, matches[:10], 0, flags=2)
+
+                # show result
+                if args.filtering:
+                    frame = both.copy()
+
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            else:
+                print("Not enough matches found - %d/%d" %
+                      (len(matches), MIN_MATCHES))
+        except:
+            break
+
+    if args.save:
+        save_data(DATA_FILE, data_headers, data_to_save)
 
     cap.release()
     cv2.destroyAllWindows()
     return 0
+
 
 def render(img, obj, projection, model, color=False):
     """
@@ -122,6 +214,7 @@ def render(img, obj, projection, model, color=False):
 
     return img
 
+
 def projection_matrix(camera_parameters, homography):
     """
     From the camera calibration matrix and the estimated homography
@@ -142,12 +235,15 @@ def projection_matrix(camera_parameters, homography):
     c = rot_1 + rot_2
     p = np.cross(rot_1, rot_2)
     d = np.cross(c, p)
-    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
-    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d /
+                   np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d /
+                   np.linalg.norm(d, 2), 1 / math.sqrt(2))
     rot_3 = np.cross(rot_1, rot_2)
     # finally, compute the 3D projection matrix from the model to the current frame
     projection = np.stack((rot_1, rot_2, rot_3, translation)).T
     return np.dot(camera_parameters, projection)
+
 
 def hex_to_rgb(hex_color):
     """
@@ -160,15 +256,39 @@ def hex_to_rgb(hex_color):
 
 # Command line argument parsing
 # NOT ALL OF THEM ARE SUPPORTED YET
-parser = argparse.ArgumentParser(description='Augmented reality application')
-
-parser.add_argument('-r','--rectangle', help = 'draw rectangle delimiting target surface on frame', action = 'store_true')
-parser.add_argument('-mk','--model_keypoints', help = 'draw model keypoints', action = 'store_true')
-parser.add_argument('-fk','--frame_keypoints', help = 'draw frame keypoints', action = 'store_true')
-parser.add_argument('-ma','--matches', help = 'draw matches between keypoints', action = 'store_true')
 # TODO jgallostraa -> add support for model specification
-#parser.add_argument('-mo','--model', help = 'Specify model to be projected', action = 'store_true')
-
+parser = argparse.ArgumentParser(description='Augmented reality demo')
+parser.add_argument(
+    '-r',
+    '--rectangle',
+    help='draw rectangle delimiting target surface on frame',
+    action='store_true'
+)
+parser.add_argument(
+    '-k',
+    '--keypoints',
+    help='draw frame and model keypoints',
+    action='store_true'
+)
+parser.add_argument(
+    '-m',
+    '--matches',
+    help='draw matches between keypoints',
+    action='store_true'
+)
+parser.add_argument(
+    '-f',
+    '--filtering',
+    help='filter output via a Kalman filter',
+    action='store_true'
+)
+parser.add_argument(
+    '-s',
+    '--save',
+    help='Save position estimation data',
+    action='store_true'
+)
+# parser.add_argument('-mo','--model', help = 'Specify model to be projected', action = 'store_true')
 args = parser.parse_args()
 
 if __name__ == '__main__':
---
+++ src/kalman.py
@@ -0,0 +1,136 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class KalmanFilter(ABC):
+    """
+    Generic Kalman Filter implementation
+    """
+
+    def __init__(self):
+        # Variables to store the current state of the Kalman filter
+        self.x = None  # Predicted state
+        self.P = None  # Prediced noise
+        # self.z = None  # Measurements -- Observations are not required as part of the state
+        self._A = None  # Process model
+        self._H = None  # Measurement model
+        self._Q = None  # Process noise
+        self._R = None  # Measurament noise
+        self._K = None  # Kalman gain
+
+    def get_current_state(self):
+        return self.x
+
+    def get_current_noise(self):
+        return self.P
+
+    @abstractmethod
+    def get_A(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_H(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_Q(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_R(self, **kwargs):
+        pass
+
+    def reset(self):
+        """Reset filter"""
+        self.__init__()
+
+    def init(self, x, P):
+        """Initial system state"""
+        self.x = x
+        self.P = P
+
+    def __update_models_and_noise(self, **kwargs):
+        """ If any of the models has to be updated with some data, do it here"""
+        self._A = self.get_A(**kwargs)
+        self._H = self.get_H(**kwargs)
+        self._Q = self.get_Q(**kwargs)
+        self._R = self.get_R(**kwargs)
+
+    def __project_state(self):
+        """ x_k = A*x_(k-1) + B*u_k"""
+        self.x = np.matmul(self._A, self.x)
+
+    def __project_covariance(self):
+        """ P_k = A*P_(k-1)*A' + Q """
+        self.P = np.matmul(
+            np.matmul(self._A, self.P),
+            np.transpose(self._A)
+        ) + self._Q
+
+    def __compute_gain(self):
+        """ K_k = P_k*H'*(H*P_K*H' + R)^-1 """
+        self.K = np.matmul(
+            np.matmul(self.P, np.transpose(self._H)),
+            np.linalg.inv(
+                np.matmul(
+                    self._H,
+                    np.matmul(self.P, np.transpose(self._H))
+                ) + self._R
+            )
+        )
+
+    def __correct_state(self, z):
+        """ x_k = x_K + K_k*(z_k - H*x_k) """
+        self.x = self.x + np.matmul(
+            self.K,
+            z - np.matmul(self._H, self.x)
+        )
+
+    def __correct_covariance(self):
+        """ P_k = (I - K_k*H)*P_k """
+        self.P = np.matmul(np.eye(16) - np.matmul(self.K, self._H), self.P)
+
+    def predict(self, **kwargs):
+        """ Prediction step """
+        self.__update_models_and_noise(**kwargs)
+        self.__project_state()
+        self.__project_covariance()
+
+    def correct(self, measurements):
+        """ Correction step """
+        self.__compute_gain()
+        self.__correct_state(measurements)
+        self.__correct_covariance()
+
+
+class KalmanTracker(KalmanFilter):
+    """
+        As a first test, track the four corners of the reference surface.
+        This means that we have 16 states (2 pos, 2 vels for each corner)
+        Dimensions are then:
+        x -> 16 x 1 (States - 4 corners (u, v) and its velocities (u', v') => 16 values (8 pos + 8 vel))
+        z -> 8 x 1 (Observations - 4 corners => 8 values)
+        A -> 16 x 16 (State transition model)
+        H -> 8 x 16 (Observation model)
+        P -> 16 x 16 (Error covariance)
+        Q -> 16 x 16 (Process noise covariance)
+        R -> 8 x 8 (Observation noise covariance)
+        K -> 16 x 8 (Kalman Gain)
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def get_A(self, dt=None):
+        return np.eye(16) + np.diag(np.ones(8), 8)*dt  # 16x16
+
+    def get_H(self, **kwargs):
+        return np.concatenate((np.eye(8), np.zeros([8, 8])), 1)  # 8x16
+
+    def get_Q(self, q=0.3, **kwargs):
+        a = np.eye(8)*q**2
+        b = np.zeros([8, 8])
+        return np.block([[b, b], [b, a]])  # 16x16
+
+    def get_R(self, r=0.6, **kwargs):
+        return np.eye(8) * r**2  # 8x8
